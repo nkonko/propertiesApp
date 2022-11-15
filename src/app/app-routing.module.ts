3fbe581b80c82b227a0eacfc73f135bdf8bd8def
nazzara <nazzara@hexacta.com>
2022-11-15 15:44:01 +0100

    added static pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'static',
+        loadChildren: () => import('./pages/static/static.module').then((m) => m.StaticModule)
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'static/welcome'
+      }
+    ]
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo:'static/404'
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
